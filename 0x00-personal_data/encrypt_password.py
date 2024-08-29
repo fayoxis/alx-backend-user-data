ac9abb0b77ca0ac4a0719d61fb47244e2dbd0825
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-08-29 19:44:02 +0100

    Update encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,32 +1,16 @@
 #!/usr/bin/env python3
-
+"""A module for encrypting passwords.
 """
-This module provides functionality for password encryption.
-"""
-
 import bcrypt
 
+
 def hash_password(password: str) -> bytes:
-    """
-    Generates a salted hash of the provided password.
-    
-    Args:
-        plain_text (str): The password to be encrypted.
-    
-    Returns:
-        bytes: The salted hash of the password.
+    """Hashes a password using a random salt.
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
+
 def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Checks is a hashed password was formed from the given password.
     """
-    Verifies if a candidate password matches the stored hash.
-    
-    Args:
-        stored_hash (bytes): The previously hashed password.
-        candidate_password (str): The password to be verified.
-    
-    Returns:
-        bool: True if the password matches the hash, False otherwise.
-    """
-      return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
