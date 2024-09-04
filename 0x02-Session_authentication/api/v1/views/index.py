146df295b988985630fce4fcf01e61bb3bf11555
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-09-04 19:36:47 +0100

    Update index.py

--- 0x02-Session_authentication/api/v1/views/index.py
+++ 0x02-Session_authentication/api/v1/views/index.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python3
-"""Module of Index views.
-"""
+"""This module defines the routes for the Index views."""
 from flask import jsonify, abort
 from api.v1.views import app_views
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
-    """GET /api/v1/status
-    Return:
-      - the status of the API.
+    """Retrieves the status of the API.
+    Returns:
+        A JSON response containing the status of the API.
     """
     return jsonify({"status": "OK"})
 
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
-    """GET /api/v1/stats
-    Return:
-      - the number of each objects.
+    """Retrieves the statistics of the application.
+    Returns:
+        A JSON response containing the count of users.
     """
     from models.user import User
     stats = {}
@@ -28,17 +27,17 @@ def stats() -> str:
 
 @app_views.route('/unauthorized/', strict_slashes=False)
 def unauthorized() -> None:
-    """GET /api/v1/unauthorized
-    Return:
-      - Unauthorized error.
+    """Raises an Unauthorized error.
+    Returns:
+        An Unauthorized error (HTTP status code 401).
     """
     abort(401)
 
 
 @app_views.route('/forbidden/', strict_slashes=False)
 def forbidden() -> None:
-    """GET /api/v1/forbidden
-    Return:
-      - Forbidden error.
+    """Raises a Forbidden error.
+    Returns:
+        A Forbidden error (HTTP status code 403).
     """
     abort(403)
