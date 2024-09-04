c658bcb77c54861ea9bc6a26bb3c4a3a866396b6
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-09-04 19:35:08 +0100

    Update __init__.py

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
-""" DocDocDocDocDocDoc
 """
+This module sets up the Blueprint for the API views
+and imports the necessary view modules and models.
+"""
+
 from flask import Blueprint
+import os
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
+# Import view modules
 from api.v1.views.index import *
 from api.v1.views.users import *
-
-User.load_from_file()
-
 from api.v1.views.session_auth import *
+
+# Load user data from file
+from models.user import User
+User.load_from_file()
