6a3e1521116eebd70ff330078de56981b28b2660
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-09-05 17:28:11 +0100

    Update auth.py

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -10,35 +10,32 @@ from flask import request
 class Auth:
     """Authentication class for managing user authentication.
     """
-
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """
-        Check if a given path requires authentication.
+        """Check if a given path requires authentication.
         bool: True if the path requires authentication,
         False otherwise.
         """
-        authenticated = True
-        if path is not None and excluded_paths is not None:
-            i = 0
-            do:
-                exclusion_path = map(lambda x: x.strip(), excluded_paths)[i]
-                pattern = ''
-                if exclusion_path[-1] == '*':
-                    pattern = '{}.*'.format(exclusion_path[0:-1])
-                elif exclusion_path[-1] == '/':
-                    pattern = '{}/*'.format(exclusion_path[0:-1])
-                else:
-                    pattern = '{}/*'.format(exclusion_path)
-                if re.match(pattern, path):
-                    authenticated = False
-                    break
-                i += 1
-            while i < len(excluded_paths)
-        return authenticated
+        if path is None or excluded_paths is None:
+            return True
+
+        i = 0
+        while i < len(excluded_paths):
+            exclusion_path = excluded_paths[i].strip()
+            pattern = ''
+            if exclusion_path[-1] == '*':
+                pattern = '{}.*'.format(exclusion_path[0:-1])
+            elif exclusion_path[-1] == '/':
+                pattern = '{}/*'.format(exclusion_path[0:-1])
+            else:
+                pattern = '{}/*'.format(exclusion_path)
+            if re.match(pattern, path):
+                return False
+            i += 1
+
+        return True
 
     def authorization_header(self, request=None) -> str:
-        """
-        Get the authorization header from the request.
+        """Get the authorization header from the request.
         request: The request object containing the authorization
         header.str: The value of the authorization header,
         or None if not present.
@@ -48,8 +45,7 @@ class Auth:
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """
-        Get the current user from the request.
+        """Get the current user from the request.
         request: The request object containing user information.
         TypeVar('User'): The current user object, or
         None if not available.
@@ -57,8 +53,7 @@ class Auth:
         return None
 
     def session_cookie(self, request=None) -> str:
-        """
-        Get the value of the session cookie from the request.
+        """Get the value of the session cookie from the request.
         request: The request object containing the session cookie.
         str: The value of the session cookie, or None if not present.
         """
