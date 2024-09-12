2809f41c33643ad5e5439da61eb9499d22981056
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-09-12 12:21:14 +0100

    Update user.py

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python3
-"""The `user` model's module.
-"""
+"""This module defines the User model for the application."""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-
+# Create a base class for declarative class definitions
 Base = declarative_base()
 
-
 class User(Base):
-    """Represents a record from the `user` table.
+    """Represents a user record in the database.
+
+    Attributes:
+        id (int): Primary key for the user record.
+        email (str): User's email address (max length: 250 characters).
+        hashed_password (str): Hashed value of the user's password
+            (max length: 250 characters).
+        session_id (str, optional): User's current session ID
+            (max length: 250 characters).
+        reset_token (str, optional): Token for resetting the user's password
+            (max length: 250 characters).
     """
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
